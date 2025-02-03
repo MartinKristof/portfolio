ca1fd2e9553f416c9de2c877c33e88cf9378f738
Martin Krištof <martin.kristof@heureka.group>
2025-02-03 16:27:19 +0100

    feat: add analytics and insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
-import "@pf/app/styles/globals.css"
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/next';
+import type React from "react"
 import { Inter } from "next/font/google"
 import { cn } from "@pf/lib/utils"
-import type React from "react"
+import "@pf/app/styles/globals.css"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -18,9 +20,13 @@ const RootLayout = (
     children: React.ReactNode
   }
 ) => (
-<html lang="en">
-  <body className={cn(inter.className, "min-h-screen bg-background antialiased")}>{children}</body>
-</html>);
+  <html lang="en">
+    <body className={cn(inter.className, "min-h-screen bg-background antialiased")}>
+      {children}
+      <SpeedInsights />
+      <Analytics />
+    </body>
+  </html>);
 
 export default RootLayout;
 
